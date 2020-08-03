2ce56d9d64c9f8b144e4900db133d1f1496235ad
Ankit <ankit@visions.net.in>
2020-08-03 09:30:45 +0530

    Pipes

---
+++ src/ng-pipes/pipes/string/ucfirst.ts
@@ -0,0 +1,12 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { isString, ucFirst } from '../helpers/helpers';
+
+@Pipe({ name: 'ucfirst' })
+export class UcFirstPipe implements PipeTransform {
+  transform(input: string): string;
+  transform(input: any): any;
+
+  transform(text: any): string {
+    return isString(text) ? ucFirst(text) : text;
+  }
+}
